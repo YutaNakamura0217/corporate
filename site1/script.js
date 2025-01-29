85175a42fb0f350c793d35517775074408f96d7f
YutaNakamura0217 <y.nakamura@tenho7.jp>
2025-01-29 21:33:24 +0900

    aaa

--- site1/script.js
+++ site1/script.js
@@ -1,17 +1,20 @@
 // スライダーの動作
-let slideIndex = 0;
-showSlides();
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function showSlides() {
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("slide");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
     slides[slideIndex - 1].style.display = "flex";
-    setTimeout(showSlides, 5000); // 5秒ごとに切り替え
 }
 
 // スティッキーヘッダー
